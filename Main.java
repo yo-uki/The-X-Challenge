d7f54ed6fa6a1970e110191e8a6aa2ef6f25fc07
Aleksandra <70322966+yo-uki@users.noreply.github.com>
2020-11-03 21:56:05 +0100

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,25 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner X = new Scanner(System.in);
+        System.out.println("Enter number:");
+
+        int x = X.nextInt();
+        X(x);
+
+    }
+
+    public static void X(int x){
+
+        X X = new X();
+        X.X(x);
+    }
+
+
+
+}
---
+++ X.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class X {
+
+    private int x=1;
+
+
+    public void X(int x){
+        while (this.x <=12){
+            System.out.println(x*this.x);
+            this.x++;
+
+        }
+    }
+
+
+}
